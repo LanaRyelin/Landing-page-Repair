cb7b2f1caa9ee232dd421f48a92495135535ffed
LanaRyelin <lanaryelin@gmail.com>
2020-12-19 18:14:53 +0500

    Сверстан и адаптирован блок с формой. Реализована валидация 2 форм с помощью плагинов jQuery Validation и jQuery Masked Input. Изменена анимация карточек

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
+// Инициализация билиотеки WOW.js
+new WOW().init();
+
 $(document).ready(function() {
+
+    // Валидация формы
+    function validateForm(form) {
+        $(form).validate({
+            rules: {
+                username: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+            },
+            messages: {
+                username: {
+                    required: "<p>Укажите ваше имя.</p>",
+                    minlength: jQuery.validator.format("<p>Имя не может быть короче {0} букв.</p>")
+                },
+                phone: "<p>Укажите ваш телефон.</p>"
+            }
+        });
+    };
+    validateForm('#offer-form');
+    validateForm('#brif-form');
+
+    // Маска для телефона
+    $('.phone').mask('+7 (999) 999-99-99');
 
     // Модальное окно
     var button = $('#button');
